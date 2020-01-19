d7c7c22f46756fc9b6026b55abffe367285ae3f8
NorthernLightDragon <42276346+NorthernLightDragon@users.noreply.github.com>
2020-01-19 18:58:57 +0100

    Add files via upload

--- CelsiusAFahrenheitConvertion/CelsiusAFahrenheitConvertion/Program.cs
+++ CelsiusAFahrenheitConvertion/CelsiusAFahrenheitConvertion/Program.cs
@@ -10,12 +10,16 @@ namespace CelsiusAFahrenheitConvertion
             int n = int.Parse(Console.ReadLine());
             Console.Write
                 ("Select an option: (1) C -> K   (2) K -> C  : ");
-            byte c = byte.Parse(Console.ReadLine());
+            byte a = byte.Parse(Console.ReadLine());
             Console.WriteLine();
-            switch(c)
+            switch(a)
             {
-
-
+                case 1:
+                    Console.WriteLine("{0} C = {1} K", n, n + 273);
+                    break;
+                case 2:
+                    Console.WriteLine("{0} K = {1} C", n, n - 273);
+                    break;
             }
             Console.ReadLine();
         }
