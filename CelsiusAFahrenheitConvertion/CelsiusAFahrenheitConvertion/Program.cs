db1ed1006808326c245ea7621c719077f1b388bc
NorthernLightDragon <42276346+NorthernLightDragon@users.noreply.github.com>
2020-01-19 18:47:34 +0100

    Add files via upload

---
+++ CelsiusAFahrenheitConvertion/CelsiusAFahrenheitConvertion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CelsiusAFahrenheitConvertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add a thermometer value: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write
+                ("Select an option: (1) C -> K   (2) K -> C  : ");
+            byte c = byte.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch(c)
+            {
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
